c2102fa8ca6d96ccf250b7b463341299172ceb7d
Crowdbotics <team@crowdbotics.com>
2021-08-08 12:40:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "weathered_leaf_29633Identifier",
+  url: "https://weathered_leaf_29633Identifier.botics.co",
+  api: "https://weathered_leaf_29633Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
